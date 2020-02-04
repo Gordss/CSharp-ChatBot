438e230d6cec6724566f70aa4e865ab6a1dd8a0e
Gordss <g.shavov@abv.bg>
2020-02-04 16:33:18 +0200

    kpi_request_data update

--- WebAPITest/Controllers/KPIRequestDataWithoutPart.cs
+++ WebAPITest/Controllers/KPIRequestDataWithoutPart.cs
@@ -7,7 +7,43 @@ namespace WebAPITest.Controllers
 {
     public class KPIRequestDataWithoutPart
     {
-        public string workOrder;
-        public string kpiType;
+
+        #region DataMembers
+        private string workOrder;
+        private string kpiType; 
+        #endregion
+
+        public string WorkOrder
+        {
+            get => workOrder;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+                else
+                {
+                    foreach (char symbol in value)
+                    {
+                        if(symbol < '0' || symbol > '9')
+                        {
+                            throw new ArgumentException();
+                        }
+                    }
+
+                    workOrder = value;
+                }
+            }
+        }
+
+        public string KpiType
+        {
+            get => kpiType;
+            set
+            {
+
+            }
+        }
+
+        public string 
     }
 }

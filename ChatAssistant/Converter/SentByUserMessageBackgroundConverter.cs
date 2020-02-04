b5d0a28162f6dbbab264c3a55bd796968fadd8da
Gordss <g.shavov@abv.bg>
2020-02-04 22:16:07 +0200

    return-back-the-WPF-project

---
+++ ChatAssistant/Converter/SentByUserMessageBackgroundConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace ChatAssistant.Converter
+{
+    /// <summary>
+    /// A converter that takes in bool and converts the message color either for user or for api response
+    /// </summary>
+    public class SentByUserMessageBackgroundConverter : BaseValueConverter<SentByUserMessageBackgroundConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? new BrushConverter().ConvertFrom("#388E3C") : new BrushConverter().ConvertFrom("#7f7f7f");
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChatAssistant/MessageBox/ChatMessageList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace ChatAssistant.MessageBox
+{
+    /// <summary>
+    /// The chat message list as class representation
+    /// </summary>
+    public class ChatMessageList
+    {
+        /// <summary>
+        /// List of chat messages
+        /// </summary>
+        public ObservableCollection<ChatMessageListItem> Items { get; set; }
+    }
+}
